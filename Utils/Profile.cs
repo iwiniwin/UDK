d72dfc61de6228edda7ce3eb6ef81c97aac4d22b
iwiniwin <iwiniwin@foxmail.com>
2019-11-29 01:14:16 +0800

    添加函数执行耗时统计接口

---
+++ Utils/Profile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UKit.Utils{
+    public class ProfileTime
+    {
+        public double startTime;
+        public void start(){
+            this.startTime = DateTime.Now.ToUniversalTime().Ticks;
+        }
+        public double stop(){
+            return (DateTime.Now.ToUniversalTime().Ticks - this.startTime) / 10000.0 / 1000.0;
+        }
+    }
+    public class Profile
+    {
+        public static ProfileTime Time;
+
+        static Profile(){
+            Time = new ProfileTime();
+        }
+
+        private Profile(){
+
+        }
+    }
+}
+
+
