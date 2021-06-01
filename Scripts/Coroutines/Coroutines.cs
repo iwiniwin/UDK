0a2f3126192c4dc05cd23e4fb7404cdea8d3ff3d
iwiniwin <iwiniwin@foxmail.com>
2021-06-01 20:07:24 +0800

    完善协程调度

--- Scripts/Coroutines/Coroutines.cs
+++ Scripts/Coroutines/Coroutines.cs
@@ -8,13 +8,13 @@ namespace UDK.Coroutines
 {
     public sealed class Coroutines
     {
-        private static Coroutines Instance = null;
+        private static Coroutines instance = null;
+        private static int lastFrameCount = 0;
+
+        private Coroutines() { }
 
         private Dictionary<string, List<Coroutine>> m_CoroutineDict = new Dictionary<string, List<Coroutine>>();
-        private Coroutines()
-        {
 
-        }
         public static Coroutine Start(IEnumerator routine, object target = null)
         {
             return InternalStart(new Coroutine(routine, target));
@@ -51,17 +51,12 @@ namespace UDK.Coroutines
 
         private static Coroutine InternalStart(Coroutine coroutine)
         {
-            if(Instance == null)
+            if (instance == null)
             {
-                Instance = new Coroutines();
+                instance = new Coroutines();
             }
-            Instance.Start(coroutine);
+            instance.Start(coroutine);
             return coroutine;
-        }
-
-        private void Start(Coroutine coroutine)
-        {
-
         }
 
         public static void Stop(IEnumerator routine, object target)
@@ -88,35 +83,118 @@ namespace UDK.Coroutines
         {
 
         }
+
+        public static void Update()
+        {
+            if (instance == null) return;
+            if(lastFrameCount == Time.frameCount)
+            {
+                Debug.LogError("Coroutines.Update is called multiple times in one frame");
+            }
+            lastFrameCount = Time.frameCount;
+            instance.OnUpdate(Time.deltaTime);
+        }
+
+        private void Start(Coroutine coroutine)
+        {
+            if (!m_CoroutineDict.ContainsKey(coroutine.Key))
+            {
+                m_CoroutineDict.Add(coroutine.Key, new List<Coroutine>());
+            }
+            m_CoroutineDict[coroutine.Key].Add(coroutine);
+            MoveNext(coroutine);
+        }
+
+        private void OnUpdate(float dt)
+        {
+            if (m_CoroutineDict.Count == 0) return;
+            // Debug.Log(m_CoroutineDict.Count + "  m_CoroutineDict.Count");
+            List<string> keys = new List<string>(m_CoroutineDict.Keys);
+            foreach (string key in keys)
+            {
+                List<Coroutine> coroutines = m_CoroutineDict[key];
+                for (int i = coroutines.Count - 1; i >= 0; i--)
+                {
+                    Coroutine coroutine = coroutines[i];
+                    if(!coroutine.CurrentYield.IsDone())
+                    {
+                        continue;
+                    }
+                    if(!MoveNext(coroutine))
+                    {
+                        coroutines.RemoveAt(i);
+                        coroutine.CurrentYield = null;
+                        coroutine.Finished = true;
+                    }
+                }
+                if(m_CoroutineDict[key].Count == 0)
+                {
+                    m_CoroutineDict.Remove(key);
+                }
+            }
+        }
+
+        private bool MoveNext(Coroutine coroutine)
+        {
+            if (!coroutine.Routine.MoveNext())
+            {
+                return false;
+            }
+            object current = coroutine.Routine.Current;
+            if (current == null)
+            {
+                coroutine.CurrentYield = new YieldDefault();
+            }
+            else if(current is Coroutine)
+            {
+                coroutine.CurrentYield = new YieldNestedCoroutine(){
+                    coroutine = (Coroutine)current
+                };
+            }
+            else if(current is IEnumerator)
+            {
+                Coroutine nestedCoroutine = Start((IEnumerator)current);
+                coroutine.CurrentYield = new YieldNestedCoroutine(){
+                    coroutine = nestedCoroutine
+                };
+            }
+            else
+            {
+                coroutine.CurrentYield = new YieldDefault();
+            }
+            return true;
+        }
     }
 
     public sealed class Coroutine : YieldInstruction
     {
-        private int m_Key = 0;
-        private int m_TargetKey = 0;
+        private string m_Key = "";
+        private string m_TargetKey = "";
 
-        public int Key => m_Key;
-        public int TargetKey => m_TargetKey; 
-        
+        public string Key => m_Key;
+        public string TargetKey => m_TargetKey;
+
         private IEnumerator m_Routine;
-        internal IEnumerator Routine => m_Routine; 
+        internal IEnumerator Routine => m_Routine;
+
+        internal bool Finished = false;
 
         internal ICoroutineYield CurrentYield = new YieldDefault();
 
         internal Coroutine(IEnumerator routine, string methodName, object target)
         {
-            this.m_Key = methodName.GetHashCode();
-            this.m_TargetKey = target.GetHashCode();
+            this.m_TargetKey = target.GetHashCode().ToString();
+            this.m_Key = this.m_TargetKey + methodName;
             this.m_Routine = routine;
         }
 
         internal Coroutine(IEnumerator routine, object target)
         {
-            this.m_Key = routine.GetHashCode();
-            if(target != null)
+            if (target != null)
             {
-                this.m_TargetKey = target.GetHashCode();
+                this.m_TargetKey = target.GetHashCode().ToString();
             }
+            this.m_Key = this.m_TargetKey + routine.GetHashCode().ToString();
             this.m_Routine = routine;
         }
     }
@@ -131,6 +209,15 @@ namespace UDK.Coroutines
         public bool IsDone()
         {
             return true;
+        }
+    }
+
+    public class YieldNestedCoroutine : ICoroutineYield
+    {
+        public Coroutine coroutine;
+        public bool IsDone()
+        {
+            return coroutine.Finished;
         }
     }
 }
